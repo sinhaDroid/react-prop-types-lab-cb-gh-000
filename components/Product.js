abc465a33dc35f735b80e4ac4a25995a7865441e
Deepanshu Sinha <107sinha@gmail.com>
2017-04-11 13:03:06 +0000

    Done.

---
+++ components/Product.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default class Product extends React.Component {
+  render() {
+    return (
+      <div className="product">
+        <p>Name: {this.props.name}</p>
+        <p>{this.props.producer ? <small>{this.props.producer}</small> : null}</p>
+        <p>{this.props.hasWatermark ? 'Watermarked' : 'Not watermarked'}</p>
+        <p>Color : {this.props.color}</p>
+        <p>Weight: {this.props.weight}</p>
+      </div>
+    );
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+Product.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  producer: React.PropTypes.string,
+  hasWatermark: React.PropTypes.bool,
+  color: React.PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: (props, propName) => {
+    const weight = props[propName];
+
+    if (weight === undefined) {
+      return new Error('The `weight` prop is required.');
+    }
+
+    if (isNaN(weight)) {
+      return new Error('The `weight` prop is not a number.');
+    }
+
+    const isValidWeight = weight > 80 && weight < 300;
+
+    if (!isValidWeight) {
+      return new Error('The `weight` prop should range between 80 and 300.');
+    }
+  }
+};
